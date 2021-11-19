0fb52f0a76e2a19a95e1d443a88faf9872beb3be
zhangjinling <zhangjinling@wenxiang.org>
2021-11-19 18:28:13 +0800

    styles all css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,31 @@ import React, { Component } from "react";
 import moduleCss from "./Home.Module.scss";
 import { Title } from "../styled/Home.styled";
 export default class Home extends Component {
+  myAlert = (str) => {
+    alert(str);
+  };
+  a = 1111;
   render() {
     return (
       <div>
         Home page
-        <Title>../styled/Home.styled</Title>
+        <Title>
+          ../styled/Home.styled
+          <p className="name">title 下的 name</p>
+          <span
+            className="item"
+            onClick={() => {
+              this.myAlert(this.a);
+            }}
+          >
+            1={this.a}
+          </span>
+          <span className="item">2</span>
+          <span className="item">3</span>
+          <span className="item">4</span>
+          <span className="item">5</span>
+          <span className="item">6</span>
+        </Title>
         <a className={moduleCss.alink} href="http://baidu.com">
           aaaaa
         </a>
--- src/styled/Home.styled.js
+++ src/styled/Home.styled.js
@@ -5,4 +5,19 @@ export const Title = styled.h1`
   color: red;
   margin-top: 20px;
   display: inline;
+  .name {
+    color: blue;
+    font-size: 30px;
+  }
+  .item {
+    display: inline-block;
+    width: 50px;
+    height: 50px;
+    line-height: 50px;
+    text-align: center;
+    margin: 5px;
+    border-radius: 4px;
+    border: 1px solid red;
+    cursor: pointer;
+  }
 `;
